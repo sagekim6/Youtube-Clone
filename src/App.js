992a373fe1069da54aed4a15192f00e84eae1d9e
KimSeYoung <sagekim6@gmail.com>
2022-07-30 22:11:05 +0900

    Create .env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const getData = async () => {
+    const res = await fetch();
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <div className="App">Start Youtube clone!</div>;
 }
 
