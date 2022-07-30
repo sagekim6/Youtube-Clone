a09f50f12d005b1fcc0aa75a587ef20e69924e4d
KimSeYoung <sagekim6@gmail.com>
2022-07-30 22:34:11 +0900

    Fetch로 데티어 받아오기

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const key = process.env.REACT_APP_YOUTUBE_API_KEY;
+
   const getData = async () => {
-    const res = await fetch();
+    const res = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${key}`
+    );
+    const json = await res.json();
+    return json;
   };
 
   useEffect(() => {
-    getData();
+    console.log(getData());
   }, []);
 
   return <div className="App">Start Youtube clone!</div>;
